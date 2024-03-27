1fbed78ed9462bee5c02f160e52342c968b1a0c1
LEEhodu <163502525+LEEhodu@users.noreply.github.com>
2024-03-27 00:52:43 +0000

    third

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import steamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = st.slider("select a value")
-st.write(x, 'is a square ', x * x)
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
+
